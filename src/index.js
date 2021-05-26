0f9c4194ab55e5a2a6e05562a8e40a45fa39dcae
Tiago Couto <strife.tiago@gmail.com>
2021-05-26 22:27:39 +0000

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const rowStyle = {
+    display: 'flex'
+}
+
+const squareStyle = {
+    'width':'60px',
+    'height':'60px',
+    'backgroundColor': '#ddd',
+    'margin': '4px',
+    'display': 'flex',
+    'justifyContent': 'center',
+    'alignItems': 'center',
+    'fontSize': '20px',
+    'color': 'white'
+}
+
+const boardStyle = {
+    'backgroundColor': '#eee',
+    'width': '208px',
+    'alignItems': 'center',
+    'justifyContent': 'center',
+    'display': 'flex',
+    'flexDirection': 'column',
+    'border': '3px #eee solid'
+}
+
+const containerStyle = {
+    'display': 'flex',
+    'alignItems': 'center',
+    'flexDirection': 'column'
+}
+
+const instructionsStyle = {
+    'marginTop': '5px',
+    'marginBottom': '5px',
+    'fontWeight': 'bold',
+    'fontSize': '16px',
+}
+
+const buttonStyle = {
+    'marginTop': '15px',
+    'marginBottom': '16px',
+    'width': '80px',
+    'height': '40px',
+    'backgroundColor': '#8acaca',
+    'color': 'white',
+    'fontSize': '16px',
+}
+
+class Square extends React.Component {
+    render() {
+        return (
+            <div
+                className="square"
+                style={squareStyle}>
+            </div>
+        );
+    }
+}
+
+class Board extends React.Component {
+    render() {
+        return (
+            <div style={containerStyle} className="gameBoard">
+                <div id="statusArea" className="status" style={instructionsStyle}>Next player: <span>X</span></div>
+                <div id="winnerArea" className="winner" style={instructionsStyle}>Winner: <span>None</span></div>
+                <button style={buttonStyle}>Reset</button>
+                <div style={boardStyle}>
+                    <div className="board-row" style={rowStyle}>
+                        <Square />
+                        <Square />
+                        <Square />
+                    </div>
+                    <div className="board-row" style={rowStyle}>
+                        <Square />
+                        <Square />
+                        <Square />
+                    </div>
+                    <div className="board-row" style={rowStyle}>
+                        <Square />
+                        <Square />
+                        <Square />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
